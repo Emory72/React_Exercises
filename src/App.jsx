727f001bc3298ab8b2d83751d1540c76f09b47b7
Emory72 <hatusan1999@gmail.com>
2023-08-12 00:56:33 +0700

    react ex first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Home from "./Web_Layout/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/Web_Layout/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Banner from "./Banner";
+import Content from "./Content";
+import Footer from "./Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
